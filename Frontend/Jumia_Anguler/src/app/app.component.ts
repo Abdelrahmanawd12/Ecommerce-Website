bae640768a1243986bfc3b4324620bf5260f8334
abdelrahman <bodeawad155@gmail.com>
2025-04-11 01:21:44 +0200

    without yassmin

--- Frontend/Jumia_Anguler/src/app/app.component.ts
+++ Frontend/Jumia_Anguler/src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../Components/header/header.component';
 import { ShopComponent } from '../Components/Cstomer/shop/shop.component';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,HttpClientModule,CommonModule],
   providers: [],
   standalone: true,
   templateUrl: './app.component.html',
