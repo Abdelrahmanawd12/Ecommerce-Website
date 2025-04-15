3fb87f422f0907d9123a642332c573d9a62eda9c
2ra2ora <raniarabea01@gmail.com>
2025-04-15 04:26:12 +0200

    Seller Repository , get sellerInfo Action

---
+++ Backend/Jumia_Api/Jumia_Api/DTOs/SellerDTOs/sellerDTO.cs
@@ -0,0 +1,20 @@
+﻿using Jumia.Models;
+
+namespace Jumia_Api.DTOs.SellerDTOs
+{
+    public class sellerDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public DateTime DOB { get; set; }
+        public Address Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string StoreName { get; set; }
+        public string ShippingZone { get; set; }
+        public string StoreAddress { get; set; }
+    }
+}
---
+++ Backend/Jumia_Api/Jumia_Api/Repository/SellerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Jumia.Data;
+using Jumia.Models;
+
+namespace Jumia_Api.Repository
+{
+    public class SellerRepository : GenericRepository<Seller>
+    {
+        public SellerRepository(JumiaDbContext context) : base(context)
+        {
+        }
+
+        public Seller GetSellerById(string id)
+        {
+            return (Seller)db.Users.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
