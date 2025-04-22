f49ec79bef34473f7672e7f52b08aa03fdcbf813
AhmedAbdou <ahmedmabdou10@gmail.com>
2025-04-22 12:55:42 +0200

    last version of Checkout

--- Backend/Jumia_Api/Jumia_Api/DTOs/CustomerDTOs/OrderConfirmationDto.cs
+++ Backend/Jumia_Api/Jumia_Api/DTOs/CustomerDTOs/OrderConfirmationDto.cs
@@ -1,8 +1,40 @@
-﻿namespace Jumia_Api.DTOs.CustomerDTOs
+﻿using Jumia_Api.DTOs.SellerDTOs;
+
+namespace Jumia_Api.DTOs.CustomerDTOs
 {
     public class OrderConfirmationDto
     {
         public string CustomerId { get; set; }
         public string ShippingAddress { get; set; }
+
+        //// Additional properties needed from Angular interface
+        //public DateTime ShippingDate { get; set; } // Root level
+        //public string DeliveryStatus { get; set; }
+
+        //public ShippingDto ShippingInfo { get; set; }
+        //public PaymentDto Payment { get; set; }
     }
+
+
+    //public class ShippingDto
+    //{
+    //    public string ShippingMethod { get; set; }
+    //    public string ShippingAddress { get; set; }
+    //    public DateTime ShippingDate { get; set; }
+    //    public DateTime DeliveryDate { get; set; }
+    //    public int TrackingNumber { get; set; }
+    //    public string ShippingStatus { get; set; }
+    //    public string ReceiverName { get; set; }
+    //    public string ReceiverPhone { get; set; }
+    //    public string ReceiverEmail { get; set; }
+    //}
+
+    //public class PaymentDto
+    //{
+    //    public string PaymentMethod { get; set; }
+    //    public string Status { get; set; }
+    //    public string TransactionId { get; set; }
+    //    public DateTime TransactionDate { get; set; }
+    //    public decimal Amount { get; set; }
+    //}
 }
